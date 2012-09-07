6cc348d200330693e21d11657a86e20b1ae1d0be
Liu Jun <liuj47@gmail.com>
2012-09-07 11:35:07 +0800

    re init

---
+++ src/com/review/algorithm/Algorithm.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.review.algorithm;
+
+/**
+ * @author Liu jun
+ *
+ */
+public class Algorithm {
+
+}
--- src/com/review/algorithm/tests/Test.java
+++ src/com/review/algorithm/tests/Test.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.review.algorithm.test;
+package com.review.algorithm.tests;
 
 /**
  * @author Liu jun
